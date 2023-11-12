b887e6129809573d8b98fa2ba37806d06bf54dc2
Keve Lajsz <kevelajsz@gmail.com>
2023-11-12 02:47:14 +0100

    fix tab layout URL persistence

--- library-client/src/categorisation/pages/CategorisationPage.tsx
+++ library-client/src/categorisation/pages/CategorisationPage.tsx
@@ -34,39 +34,63 @@ import { MaterialSymbol } from "@/utils/components";
 import { useCategorisationObjectDetails } from "@/categorisation/hooks";
 import { categorisationTypeName } from "@/categorisation";
 
+const PARAM_KEY_TAB = "tab";
+const PARAM_VALUE_TAB_CATEGORIES = "categories";
+const PARAM_VALUE_TAB_AUTHORS = "authors";
+const PARAM_VALUE_TAB_PUBLISHERS = "publishers";
+const PARAM_KEY_MODAL = "modal";
+const PARAM_VALUE_MODAL_NEW = "new";
+const PARAM_KEY_TYPE = "type";
+const PARAM_KEY_ID = "id";
+
 const tabs: {
     label: string
     value: string
     type: CategorisationType
 }[] = [ {
     label: "Kategóriák",
-    value: "categories",
+    value: PARAM_VALUE_TAB_CATEGORIES,
     type: "categories"
 }, {
     label: "Írók",
-    value: "authors",
+    value: PARAM_VALUE_TAB_AUTHORS,
     type: "authors"
 }, {
     label: "Kiadók",
-    value: "publishers",
+    value: PARAM_VALUE_TAB_PUBLISHERS,
     type: "publishers"
 } ];
 
-const PARAM_KEY_MODAL = "modal";
-const PARAM_VALUE_MODAL_NEW = "new";
-const PARAMS_KEY_TYPE = "type";
-const PARAMS_KEY_ID = "id";
-
 export default function CategorisationPage() {
     const library = useLibraryFromContext();
+    const [ searchParams, setSearchParams ] = useSearchParams();
+
+    const defaultTab = useMemo(() => {
+        if (!searchParams.has(PARAM_KEY_TAB)) {
+            return PARAM_VALUE_TAB_CATEGORIES;
+        }
+
+        const paramTab = searchParams.get(PARAM_KEY_TAB)!;
+        if (![ PARAM_VALUE_TAB_CATEGORIES, PARAM_VALUE_TAB_AUTHORS, PARAM_VALUE_TAB_PUBLISHERS ]
+            .includes(paramTab)) {
+            return PARAM_VALUE_TAB_CATEGORIES;
+        }
+
+        return paramTab;
+    }, [ searchParams ]);
 
     return (
         <Card>
-            <Tabs value="categories">
+            <Tabs value={ defaultTab }>
                 <CardHeader floated={ false } shadow={ false } className="rounded-none m-6">
                     <TabsHeader>
                         { tabs.map(({ value, label }, index) => (
-                            <Tab value={ value } key={ index }>
+                            <Tab value={ value } key={ index } onClick={ () => {
+                                setSearchParams(prevState => {
+                                    prevState.set(PARAM_KEY_TAB, value);
+                                    return prevState;
+                                });
+                            } }>
                                 { label }
                             </Tab>
                         )) }
@@ -96,10 +120,10 @@ function Content<T extends Identifiable & { name: string }>(props: {
     const openModal = useCallback((id?: number) => {
         setSearchParams(prevState => {
             prevState.set(PARAM_KEY_MODAL, PARAM_VALUE_MODAL_NEW);
-            prevState.set(PARAMS_KEY_TYPE, props.type);
+            prevState.set(PARAM_KEY_TYPE, props.type);
 
             if (!!id) {
-                prevState.set(PARAMS_KEY_ID, String(id));
+                prevState.set(PARAM_KEY_ID, String(id));
             }
 
             return prevState;
@@ -145,18 +169,18 @@ function Modal(props: { type: CategorisationType }) {
 
     const open = useMemo(() => {
         return searchParams.get(PARAM_KEY_MODAL) === PARAM_VALUE_MODAL_NEW &&
-            searchParams.get(PARAMS_KEY_TYPE) === props.type;
+            searchParams.get(PARAM_KEY_TYPE) === props.type;
     }, [ props.type, searchParams ]);
 
     const setOpen = useCallback((isOpen: boolean) => {
         setSearchParams(prevState => {
             if (isOpen) {
                 prevState.set(PARAM_KEY_MODAL, PARAM_VALUE_MODAL_NEW);
-                prevState.set(PARAMS_KEY_TYPE, props.type);
+                prevState.set(PARAM_KEY_TYPE, props.type);
             } else {
                 prevState.delete(PARAM_KEY_MODAL);
-                prevState.delete(PARAMS_KEY_TYPE);
-                prevState.delete(PARAMS_KEY_ID);
+                prevState.delete(PARAM_KEY_TYPE);
+                prevState.delete(PARAM_KEY_ID);
             }
 
             return prevState;
@@ -166,8 +190,8 @@ function Modal(props: { type: CategorisationType }) {
     }, [ props.type, setSearchParams ]);
 
     const [ catObj, loadingCatObj ] = useCategorisationObjectDetails(props.type,
-        searchParams.has(PARAMS_KEY_ID) && open ?
-            parseInt(searchParams.get(PARAMS_KEY_ID)!) : -1
+        searchParams.has(PARAM_KEY_ID) && open ?
+            parseInt(searchParams.get(PARAM_KEY_ID)!) : -1
     );
 
     const [ name, setName ] = useState("");
@@ -177,14 +201,14 @@ function Modal(props: { type: CategorisationType }) {
         if (!name) return;
 
         setLoading(true);
-        const id = searchParams.has(PARAMS_KEY_ID) ?
-            parseInt(searchParams.get(PARAMS_KEY_ID)!) :
+        const id = searchParams.has(PARAM_KEY_ID) ?
+            parseInt(searchParams.get(PARAM_KEY_ID)!) :
             undefined;
         const promise = !id ? createCategorizationObject(props.type, name) :
             editCategorisationObject(props.type, id, name);
 
         promise.then(() => {
-            const action = searchParams.has(PARAMS_KEY_ID) ? "módosítva" : "létrehozva";
+            const action = searchParams.has(PARAM_KEY_ID) ? "módosítva" : "létrehozva";
             displayInfoNotification(`${ categorisationTypeName(props.type) } ${ action }`);
         }).catch(displayErrorNotification).finally(() => {
             setOpen(false);
@@ -203,7 +227,7 @@ function Modal(props: { type: CategorisationType }) {
                     <DialogHeader>
                         <Typography variant="lead">
                             { categorisationTypeName(props.type) }&nbsp;
-                            { searchParams.has(PARAMS_KEY_ID) ? "módosítása" : "létrhozása" }
+                            { searchParams.has(PARAM_KEY_ID) ? "módosítása" : "létrhozása" }
                         </Typography>
                     </DialogHeader>
                     <hr />
@@ -218,7 +242,7 @@ function Modal(props: { type: CategorisationType }) {
                             Mégsem
                         </Button>
                         <Button variant="filled" onClick={ create } disabled={ !name || loading }>
-                            { searchParams.has(PARAMS_KEY_ID) ? "Módosítás" : "Létrehozás" }
+                            { searchParams.has(PARAM_KEY_ID) ? "Módosítás" : "Létrehozás" }
                         </Button>
                     </DialogFooter>
                 </Fragment>
