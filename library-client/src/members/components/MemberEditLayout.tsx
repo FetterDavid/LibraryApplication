841e4e9581011328a411faebcb8eda24a5def7fe
Keve Lajsz <kevelajsz@gmail.com>
2023-11-12 21:06:38 +0100

    add MemberEditLayout input types

--- library-client/src/members/components/MemberEditLayout.tsx
+++ library-client/src/members/components/MemberEditLayout.tsx
@@ -7,13 +7,13 @@ export function MemberEditLayout(props: DataEditLayoutComponentProps<MemberEditD
         <div className="w-full flex flex-col items-stretch gap-2">
             <Input crossOrigin="" value={ props.data.name } onChange={ event => {
                 props.onData({ ...props.data, name: event.target.value });
-            } } label="Név" />
+            } } label="Név" type="text" />
             <Input crossOrigin="" value={ props.data.email } onChange={ event => {
                 props.onData({ ...props.data, email: event.target.value });
-            } } label="E-mail cím" />
+            } } label="E-mail cím" type="email" />
             <Input crossOrigin="" value={ props.data.membershipType } onChange={ event => {
                 props.onData({ ...props.data, membershipType: event.target.value });
-            } } label="Tagság típusa" />
+            } } label="Tagság típusa" type="text" />
         </div>
     );
 }
